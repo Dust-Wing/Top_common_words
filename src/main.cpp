47b671208c9d57d9d3f16aabbdea2e9f695224c5
Dust-Wing <3061574442@qq.com>
2026-05-01 16:46:57 -0700

    Solve top common words

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
+#include <fstream>
 #include <iostream>
+#include "TopCommonWords.h"
 
-int main(int argc, char** argv) {
-    std::cout << "Usage: ./TopCommonWords path_to_file [num_words_to_find]" << std::endl;
+using namespace std;
 
-
+int main(int argc,char** argv){
+    if(argc<2){
+        cout<<"Usage: ./TopCommonWords path_to_file [num_words_to_find]"<<endl;
+        return 0;
+    }
+    int n=10;
+    if(argc>2){
+        n=stoi(argv[2]);
+    }
+    ifstream in(argv[1]);
+    map<string,int> m=countWords(in);
+    vector<pair<int,vector<string>>> v=topWords(m,n);
+    printTop(v,cout);
     return 0;
 }
--- testing/test_top_common_words.cpp
+++ testing/test_top_common_words.cpp
@@ -1,28 +1,50 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <ranges>
 #include "TopCommonWords.h"
 #include "gtest/gtest.h"
 #include "rapidcheck/gtest.h"
 
-namespace r = std::ranges;
-namespace rv = r::views;
+TEST(TopCommonWordsTest,CleanWordWorks){
+    EXPECT_EQ(cleanWord("HELLO"),"hello");
+    EXPECT_EQ(cleanWord("!!$#%hello<>?/"),"hello");
+    EXPECT_EQ(cleanWord("123abc!!!"),"123abc");
+    EXPECT_EQ(cleanWord("!!!"),"");
+}
 
-TEST(Example, StringStreamInputGeneration) {
-    //test content
-    const std::string content("Hope you are learning a lot.");
-    std::stringstream input_stream(content); //make a string stream over the content
-    std::vector<std::string> answer{ //this is just to have something to check against
-        "Hope", "you", "are",
-        "learning", "a", "lot."
-    };
+TEST(TopCommonWordsTest,SkipWordWorks){
+    EXPECT_TRUE(skipWord("a"));
+    EXPECT_TRUE(skipWord("the"));
+    EXPECT_FALSE(skipWord("hello"));
+}
 
-    //iota gives us the values 0, 1, 2, 3, ...
-    //zip allows us to iterate over the index, input words, and answers together
-    //rv::istream<std::string> causes us to read out one "word" at a time from the string stream
-    for (const auto& [index, input_word, answer_word] : rv::zip(rv::iota(0), rv::istream<std::string>(input_stream), answer)) {
-        EXPECT_EQ(input_word, answer_word) << input_word << " and " << answer_word << " differ at index " << index;
-    }
+TEST(TopCommonWordsTest,CountWordsWorks){
+    std::stringstream in("Hello hello, the THE and bob bob !!");
+    std::map<std::string,int> m=countWords(in);
+    EXPECT_EQ(m["hello"],2);
+    EXPECT_EQ(m["bob"],2);
+    EXPECT_EQ(m.count("the"),0);
+    EXPECT_EQ(m.count("and"),0);
+}
 
+TEST(TopCommonWordsTest,TopWordsKeepsTies){
+    std::map<std::string,int> m;
+    m["apple"]=5;
+    m["banana"]=5;
+    m["cat"]=4;
+    m["dog"]=3;
+    std::vector<std::pair<int,std::vector<std::string>>> v=topWords(m,1);
+    ASSERT_EQ(v.size(),1);
+    EXPECT_EQ(v[0].first,5);
+    std::vector<std::string> ans={"apple","banana"};
+    EXPECT_EQ(v[0].second,ans);
+}
+
+TEST(TopCommonWordsTest,PrintTopWorks){
+    std::vector<std::pair<int,std::vector<std::string>>> v;
+    v.push_back({5,{"apple","banana"}});
+    v.push_back({3,{"dog"}});
+    std::stringstream out;
+    printTop(v,out);
+    EXPECT_EQ(out.str(),"1.) These words appeared 5 times: {apple, banana}\n2.) These words appeared 3 times: {dog}\n");
 }
